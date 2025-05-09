1c8aae6d5b7155814a9628212b672b716f4edfb5
ABardelli_GigaPiu <andrea.bardelli@gigapiu.it>
2025-05-09 13:55:15 +0200

    multiple threshold support
    - Notifications are shown only once each time the threshold is surpassed
    - 3 low battery threasholds, and relative configs

---
+++ Forms/BatteryNotificationSettingsDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BatteryNotifier.Forms {
+	public class BatteryNotificationSettingsDto {
+		public string Title { get; set; }
+		public string MessageTemplate { get; set; }
+		float threshold;
+		public float ThresholdReal { get => threshold; set => threshold = value; }
+		public bool PlaySound { get; set; }
+		public bool ShowNotification { get; set; }
+		public string Music { get; set; }
+		public TimeSpan Duration { get; set; }
+
+		public bool MaxOncePerThreshold { get; set; }
+
+		public BatteryNotificationSettingsDto(string title, string messageTemplate, float threshold, string? music, bool showNotification, TimeSpan duration, bool maxOncePerThreshold) {
+			this.Title = title;
+			this.MessageTemplate = messageTemplate ?? throw new ArgumentNullException(nameof(messageTemplate));
+			this.threshold = threshold;
+			this.Music = music;
+			this.PlaySound = !string.IsNullOrEmpty(music);
+			this.ShowNotification = showNotification;
+			this.Duration = duration;
+			this.MaxOncePerThreshold = maxOncePerThreshold;
+
+			if (!IsValidTemplate(this.MessageTemplate)) throw new ArgumentException(null, nameof(messageTemplate));
+		}
+
+		public string BuildMessage() => String.Format(MessageTemplate, ThresholdReal.ToString("0%"));
+		public static bool IsValidTemplate(string messageTemplate) => true;
+	}
+}
